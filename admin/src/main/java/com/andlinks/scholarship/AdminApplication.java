abd52256955d3f4273f51ce52b7eef8b47fdeeaf
chenyalan <1448073111@qq.com>
2017-08-28 10:46:49 +0800

    项目搭建

---
+++ admin/src/main/java/com/andlinks/scholarship/AdminApplication.java
@@ -0,0 +1,15 @@
+package com.andlinks.scholarship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 陈亚兰 on 2017/8/28.
+ */
+@SpringBootApplication
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class,args);
+    }
+
+}
